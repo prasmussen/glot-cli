01eb77367f6c0de720607e9e7d491096daa1bc22
Petter Rasmussen <petter.rasmussen@gmail.com>
2015-06-26 21:58:29 +0200

    Delete next version features

--- glot.go
+++ glot.go
@@ -21,19 +21,13 @@ var (
 
 func main() {
     cli.AddHandler("new <language>", newSnippet, "Create new snippet")
-    cli.AddHandler("snippets", listSnippets, "List snippets")
-    //cli.AddHandler("snippets --page <n>", listSnippets, "List snippets")
-    cli.AddHandler("meta <id>", printMetaSnippet, "Print snippet meta information")
-    cli.AddHandler("clone <id>", cloneSnippet, "Clone snippet into a directory")
-    cli.AddHandler("delete <id>", deleteSnippet, "Delete snippet")
-    cli.AddHandler("pull", pullSnippet, "Pull snippet from api, will overwrite local changes")
-    cli.AddHandler("push", pushSnippet, "Push snippet to api, will overwrite remote changes")
-    cli.AddHandler("languages", listLanguages, "List available languages available to run")
-    cli.AddHandler("versions <language>", listVersions, "List available versions for a language")
     cli.AddHandler("run <path>", runLatest, "Run code")
     cli.AddHandler("run <path> --version <version>", runVersion, "Run code code with a specific language version")
-    cli.AddHandler("--version", printAppVersion, "Print application version")
+    cli.AddHandler("publish", publishSnippet, "Publish snippet")
+    cli.AddHandler("languages", listLanguages, "List available languages available to run")
+    cli.AddHandler("versions <language>", listVersions, "List available versions for a language")
     cli.AddHandler("help", printHelp, "Print help")
+    cli.AddHandler("--version", printAppVersion, "Print application version")
 
     ok := cli.Handle(os.Args[1:])
     if !ok {
@@ -46,34 +40,9 @@ func newSnippet(args map[string]string) {
     snippets.NewSnippet(args["language"])
 }
 
-func printMetaSnippet(args map[string]string) {
+func publishSnippet(args map[string]string) {
     cfg := getConfig()
-    snippets.PrintMeta(cfg, args["id"])
-}
-
-func cloneSnippet(args map[string]string) {
-    cfg := getConfig()
-    snippets.Clone(cfg, args["id"])
-}
-
-func deleteSnippet(args map[string]string) {
-    cfg := getConfig()
-    snippets.Delete(cfg, args["id"])
-}
-
-func pullSnippet(args map[string]string) {
-    cfg := getConfig()
-    snippets.Pull(cfg)
-}
-
-func pushSnippet(args map[string]string) {
-    cfg := getConfig()
-    snippets.Push(cfg)
-}
-
-func listSnippets(args map[string]string) {
-    cfg := getConfig()
-    snippets.ListSnippets(cfg)
+    snippets.Publish(cfg)
 }
 
 func listLanguages(args map[string]string) {
--- snippets/snippets.go
+++ snippets/snippets.go
@@ -2,8 +2,6 @@ package snippets
 
 import (
     "fmt"
-    "strings"
-    "strconv"
     "os"
     "path/filepath"
     "io/ioutil"
@@ -44,118 +42,10 @@ func NewSnippet(lang string) {
     fmt.Printf("Created %s\n", path)
 }
 
-func ListSnippets(cfg config) {
-    url := apiurl.List(cfg.SnippetsApiBaseUrl())
-    token := cfg.SnippetsApiToken()
-    snippets, err := api.List(url, token)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to list snippets: %s\n", err.Error())
-        return
-    }
+func Publish(cfg config) {
+    fmt.Printf("Publishing...\n")
+    basePath, _ := findGlotPath()
 
-    // Define column format
-    format := "%-22s %-12s %-12s %s\n"
-
-    // Print header
-    fmt.Printf(format, "Created", "Id", "Language", "Title")
-
-    // Print snippets
-    for _, snippet := range snippets {
-        fmt.Printf(format, snippet.Created, snippet.Id, snippet.Language, snippet.Title)
-    }
-}
-
-func PrintMeta(cfg config, id string) {
-    url := apiurl.Get(cfg.SnippetsApiBaseUrl(), id)
-    snippet, err := api.Get(url)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to get snippets: %s\n", err.Error())
-        return
-    }
-
-    fmt.Printf("Id: %s\n", snippet.Id)
-    fmt.Printf("Title: %s\n", snippet.Title)
-    fmt.Printf("Language: %s\n", snippet.Language)
-    fmt.Printf("Public: %s\n", strings.Title(strconv.FormatBool(snippet.Public)))
-    fmt.Printf("Created: %s\n", snippet.Created)
-    fmt.Printf("Modified: %s\n", snippet.Modified)
-    fmt.Printf("Owner: %s\n", snippet.Owner)
-    fmt.Printf("Files hash: %s\n", snippet.FilesHash)
-    fmt.Printf("Web Url: %s\n", strings.Replace(snippet.Url, "snippets.glot.io", "glot.io", 1))
-    fmt.Printf("Api Url: %s\n", snippet.Url)
-}
-
-func Clone(cfg config, id string) {
-    basePath := id
-    fmt.Printf("Cloning into '%s'...\n", basePath)
-
-    // Check if destination dir already exists
-    if util.FileExists(basePath) {
-        fmt.Fprintf(os.Stderr, "Destination path '%s' already exists\n", basePath)
-        return
-    }
-
-    // Get snippet from api
-    url := apiurl.Get(cfg.SnippetsApiBaseUrl(), id)
-    snippet, err := api.Get(url)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to get snippets: %s\n", err.Error())
-        return
-    }
-
-    // Write snippet to disk
-    writeSnippet(basePath, snippet)
-}
-
-func Pull(cfg config) {
-    fmt.Printf("Pulling...\n")
-
-    // Read meta file
-    meta, err := findReadMetaFile()
-    if err != nil {
-        fmt.Fprintln(os.Stderr, err.Error())
-        return
-    }
-
-    // Get snippet from api
-    url := apiurl.Get(cfg.SnippetsApiBaseUrl(), meta.Id)
-    snippet, err := api.Get(url)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to get snippets: %s\n", err.Error())
-        return
-    }
-
-    // Write snippet to disk
-    writeSnippet(".", snippet)
-}
-
-func Push(cfg config) {
-    fmt.Printf("Pushing...\n")
-
-    basePath, ok := findGlotPath()
-    if !ok {
-        pushNew(cfg, basePath)
-    } else {
-        pushUpdate(cfg, basePath)
-    }
-}
-
-func Delete(cfg config, id string) {
-    fmt.Println("Deleting...")
-
-    url := apiurl.Delete(cfg.SnippetsApiBaseUrl(), id)
-    token := cfg.SnippetsApiToken()
-    err := api.Delete(url, token)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to delete snippet: %s\n", err.Error())
-        return
-    }
-
-    fmt.Printf("Done\n")
-}
-
-
-func pushNew(cfg config, basePath string) {
     f, err := os.Open(".")
     if err != nil {
         fmt.Fprintf(os.Stderr, "Failed to read files: %s\n", err.Error())
@@ -206,58 +96,4 @@ func pushNew(cfg config, basePath string) {
     }
 
     fmt.Printf("Saved snippet %s\n", snippet.Id)
-}
-
-func pushUpdate(cfg config, basePath string) {
-    // Read meta file
-    meta, err := findReadMetaFile()
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to read meta file: %s\n", err.Error())
-        return
-    }
-
-    f, err := os.Open(".")
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to read files: %s\n", err.Error())
-        return
-    }
-
-    names, err := f.Readdirnames(0)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to read files: %s\n", err.Error())
-        return
-    }
-
-    mainFile := findMainFile(names)
-
-    files, err := util.ReadFiles(mainFile)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to read files: %s\n", err.Error())
-        return
-    }
-
-    snippetData := &api.SnippetData{
-        Language: meta.Language,
-        Title: meta.Title,
-        Public: meta.Public,
-        Files: toApiFiles(files),
-    }
-
-    url := apiurl.Update(cfg.SnippetsApiBaseUrl(), meta.Id)
-    token := cfg.SnippetsApiToken()
-    snippet, err := api.Update(url, token, snippetData)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to update snippet: %s\n", err.Error())
-        return
-    }
-
-    // Write meta file to disk
-    metaFilePath := filepath.Join(basePath, ".glot", "meta")
-    err = writeMetaFile(metaFilePath, snippet.MetaSnippet)
-    if err != nil {
-        fmt.Fprintf(os.Stderr, "Failed to write meta file to disk: %s\n", err.Error())
-        return
-    }
-
-    fmt.Printf("Updated snippet %s\n", snippet.Id)
 }
